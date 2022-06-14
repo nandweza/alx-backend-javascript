50ea1e0018d8ccb58fb1cb2d0d05f1438854bea1
nandweza <allannandweza@gmail.com>
2022-06-14 16:32:55 +0000

    correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task = true;
+  task = false;
+  task2 = true;
 
   if (trueOrFalse) {
     var task = true;
-    var task = false;
+    var task2 = false;
   }
 
   return [task, task2];
