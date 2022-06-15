cf28c7ceea79113438fc65c2f6000627b38ad523
nandweza <allannandweza@gmail.com>
2022-06-15 06:55:21 +0000

    fixed code

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,5 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = {
-    income: income,
-    gdp: gdp,
-    capita: capita,
-  };
+  const budget = { income, gdp, capita };
 
   return budget;
 }
