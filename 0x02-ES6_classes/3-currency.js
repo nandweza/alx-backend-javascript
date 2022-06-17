0cbf59ee117656d3c71648e519d17394b62f82b0
Nandweza Allan <63627595+nandweza@users.noreply.github.com>
2022-06-17 12:29:50 +0300

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,29 +1,29 @@
 export default class Currency {
-    constructor(code, name) {
-	this.code = code;
-	this.name = name;
-    }
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-    // get and set code
-    get code() {
-	return this._code;
-    }
+  // get and set code
+  get code() {
+    return this._code;
+  }
 
-    set code(value) {
-	this._code = value;
-    }
+  set code(value) {
+    this._code = value;
+  }
 
-    // get and set name
-    get name() {
-	return this._name;
-    }
+  // get and set name
+  get name() {
+    return this._name;
+  }
 
-    set name(value) {
-	this._name = value;
-    }
+  set name(value) {
+    this._name = value;
+  }
 
-    // method
-    displayFullCurrency() {
-	return `${this._name} (${this._code})`;
-    }
-}
+  // method
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+} 
