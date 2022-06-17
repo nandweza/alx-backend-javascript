2419ab684041ffe2852638c187e83207640fd20a
nandweza <allannandweza@gmail.com>
2022-06-17 09:08:02 +0000

    added task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+export default class Currency {
+    constructor(code, name) {
+	this.code = code;
+	this.name = name;
+    }
+
+    // get and set code
+    get code() {
+	return this._code;
+    }
+
+    set code(value) {
+	this._code = value;
+    }
+
+    // get and set name
+    get name() {
+	return this._name;
+    }
+
+    set name(value) {
+	this._name = value;
+    }
+
+    // method
+    displayFullCurrency() {
+	return `${this._name} (${this._code})`;
+    }
+}
