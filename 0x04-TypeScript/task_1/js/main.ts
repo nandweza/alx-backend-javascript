72800b5669038fc6e618ea579c4be4081dbcb7f8
nandweza <allannandweza@gmail.com>
2022-06-23 21:42:57 +0300

    class student

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,3 +20,34 @@ interface printTeacherFunction {
 export function printTeacher(firstName: string, lastName: string): string {
     return `${firstName[0]}. ${lastName}`;
 }
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassConstructorInterface {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working'
+    }
+
+    displayName(): string {
+        return this.firstName
+    }
+}
+
+const studentClass: StudentClass = new StudentClass('Sarah', 'Mapenzi');
+console.log(studentClass.displayName());
+console.log(studentClass.workOnHomework());
