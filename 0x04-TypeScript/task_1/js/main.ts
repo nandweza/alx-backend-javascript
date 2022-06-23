8620b271c62169139c0e2b26b1ea528d79513d23
Nandweza Allan <63627595+nandweza@users.noreply.github.com>
2022-06-23 19:10:50 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -12,23 +12,3 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
-
-const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-};
-  
-console.log(teacher3);
-
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
-console.log(director1);
-
