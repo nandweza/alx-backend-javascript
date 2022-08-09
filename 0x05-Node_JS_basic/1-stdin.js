466bfadf4fd7daf9db1b9c2e0c9d4ed284849c63
nandweza <allannandweza@gmail.com>
2022-08-09 09:10:46 +0300

    fixed code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,12 +5,12 @@ console.log(message);
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null) {
-        process.stdout.write(`Your name is: ${chunk}`);
-    }
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
