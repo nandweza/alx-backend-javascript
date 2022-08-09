c489aae9c0bf2b92d2bf29a22471da921002e1e1
nandweza <allannandweza@gmail.com>
2022-08-09 10:03:59 +0300

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
